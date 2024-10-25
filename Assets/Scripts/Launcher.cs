b189e3ba4f7e24b937657fdd67150675570e9781
zhangys <313644113@qq.com>
2024-10-25 18:24:09 +0800

    initial

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Launcher : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Enter());
+    }
+    
+    IEnumerator Enter()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadSceneAsync("Main");
+    }
+}
